8eb5033830725f257d2093d063e555cbe6189a10
khaledCSE <khaledcse10@gmail.com>
2024-11-11 22:23:29 +0600

    Startup details page

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,14 @@ const nextConfig: NextConfig = {
         hostname: '*'
       }
     ]
+  },
+  experimental: {
+    ppr: true,
+  },
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: 'bottom-right',
   }
 };
 
