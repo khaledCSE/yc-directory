49095f76d942514f2e5b589f678bc6a8eecd46bb
khaledCSE <khaledcse10@gmail.com>
2024-11-12 23:18:49 +0600

    Editor picks

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -1,7 +1,6 @@
 import { formatDate } from '@/lib/utils';
 import { client } from '@/sanity/lib/client';
-import { STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
-import { Author, Startup } from '@/sanity/types';
+import { PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY } from '@/sanity/lib/queries';
 import Image from 'next/image';
 import Link from 'next/link';
 import { notFound } from 'next/navigation';
@@ -9,6 +8,7 @@ import React, { Suspense } from 'react'
 import MarkdownIt from 'markdown-it';
 import { Skeleton } from '@/components/ui/skeleton';
 import View from '@/components/View';
+import StartupCard, { StartupCardType } from '@/components/StartupCard';
 
 export const experimental_ppa = true;
 
@@ -17,7 +17,14 @@ const md = MarkdownIt()
 const StartupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id
 
-  const post: Omit<Startup, "author"> & { author: Author } = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+  const [post, { select: editorsPicks }] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'editor-s-picks' }),
+  ])
+
+  // const post: Omit<Startup, "author"> & { author: Author } = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+
+  // const { select: editorsPicks } = await client.fetch(PLAYLIST_BY_SLUG_QUERY, { slug: 'editor-s-picks' })
 
   if (!post) {
     return notFound()
@@ -60,6 +67,18 @@ const StartupPage = async ({ params }: { params: Promise<{ id: string }> }) => {
         </div>
 
         <hr className="divider" />
+
+        {editorsPicks?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor&apos;s Picks</p>
+
+            <ul className="mt-7 card_grid-sm">
+              {editorsPicks.map((post: StartupCardType, i: number) => (
+                <StartupCard key={i} post={post} />
+              ))}
+            </ul>
+          </div>
+        )}
 
         <Suspense fallback={<Skeleton className='view_skeleton' />}>
           <View id={id} />
