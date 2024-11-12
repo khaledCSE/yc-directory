e0ef42fcefdbb2e116f4d291b8b82f33c8a60ba6
khaledCSE <khaledcse10@gmail.com>
2024-11-12 22:06:32 +0600

    Navbar Responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import React from 'react'
 import { auth, signIn, signOut } from '../auth'
 import Link from 'next/link'
+import { BadgePlus, LogOut } from 'lucide-react'
+import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 
 const Navbar = async () => {
   const session = await auth()
@@ -16,7 +18,8 @@ const Navbar = async () => {
           {session && session?.user ? (
             <>
               <Link href="/startup/create">
-                <span>Create</span>
+                <span className="max-sm:hidden">Create</span>
+                <BadgePlus className="size-6 sm:hidden" />
               </Link>
 
               <form
@@ -28,12 +31,18 @@ const Navbar = async () => {
               >
                 <button type="submit">
                   <span className="max-sm:hidden">Logout</span>
-                  {/* <LogOut className="size-6 sm:hidden text-red-500" /> */}
+                  <LogOut className="size-6 sm:hidden text-red-500" />
                 </button>
               </form>
 
               <Link href={`/user/${session?.id}`}>
-                <span>{session?.user?.name}</span>
+                <Avatar className="size-10">
+                  <AvatarImage
+                    src={session?.user?.image || ""}
+                    alt={session?.user?.name || ""}
+                  />
+                  <AvatarFallback>AV</AvatarFallback>
+                </Avatar>
               </Link>
             </>
           ) : (
